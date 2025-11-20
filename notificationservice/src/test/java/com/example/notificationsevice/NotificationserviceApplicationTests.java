3990dba8cc7a4b4b0266fc78b518c7f7b8c30a99
rahulsingh-07 <bt22cse034@nituk.ac.in>
2025-11-20 12:13:02 +0530

    chore: register notification service with Eureka service registry for discovery

--- notificationservice/src/test/java/com/example/notificationsevice/NotificationserviceApplicationTests.java
+++ notificationservice/src/test/java/com/example/notificationsevice/NotificationserviceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotificationseviceApplicationTests {
+class NotificationserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
