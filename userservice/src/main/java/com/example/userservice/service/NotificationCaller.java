be790d29897e95c1a04c7f2136867a0e84275b57
rahulsingh-07 <bt22cse034@nituk.ac.in>
2025-11-21 23:21:08 +0530

    feat: implement rate limiter

--- userservice/src/main/java/com/example/userservice/service/NotificationCaller.java
+++ userservice/src/main/java/com/example/userservice/service/NotificationCaller.java
@@ -3,6 +3,7 @@ package com.example.userservice.service;
 import com.example.userservice.dto.OtpSendRequest;
 import com.example.userservice.external.service.NotificationService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,8 @@ import org.springframework.stereotype.Service;
 public class NotificationCaller {
     private final NotificationService notificationService;
 
-    @Retry(name = "notificationRetry")
+    @RateLimiter(name = "notificationRateLimiter", fallbackMethod = "rateLimitFallback")
+    @Retry(name = "notificationRetry",fallbackMethod = "fallback")
     @CircuitBreaker(name = "notificationBreaker", fallbackMethod = "fallback")
     public String sendOtp(OtpSendRequest req) {
         notificationService.sendOtp(req);  // exception caught HERE
@@ -21,5 +23,9 @@ public class NotificationCaller {
 
     public String fallback(OtpSendRequest req, Throwable ex) {
         return "Notification service is down. OTP sending failed.";
+    }
+
+    public String rateLimitFallback(OtpSendRequest req, Throwable ex) {
+        return "Too many requests. Please try again later.";
     }
 }
